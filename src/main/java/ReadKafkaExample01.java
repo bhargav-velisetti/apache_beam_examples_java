2ab5aceb786fbbbf2250cd451e9a7e8ff5c9ff93
Bhargav Velisetti <bhargava.velisetti@gmail.com>
2022-10-26 15:03:44 -0700

    added kafka streaming example

---
+++ src/main/java/ReadKafkaExample01.java
@@ -0,0 +1,50 @@
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.io.kafka.KafkaRecord;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.joda.time.Duration;
+
+public class ReadKafkaExample01 {
+
+
+    public static void main(String[] args) {
+
+        Pipeline p = Pipeline.create();
+
+        PCollection<KafkaRecord<Integer, String>> pckafka = p.apply(KafkaIO.<Integer, String>read()
+                                                        .withBootstrapServers("localhost:9092")
+                                                        .withTopic("test_producer01")
+                                                        .withKeyDeserializer(IntegerDeserializer.class)
+                                                        .withValueDeserializer(StringDeserializer.class));
+
+        pckafka.apply(ParDo.of(new PrintKafkaMessages()));
+
+        PipelineResult result = p.run();
+        result.waitUntilFinish(Duration.standardSeconds(120)); // Pipeline will run 2 mins only
+    }
+    public static class PrintKafkaMessages extends DoFn<KafkaRecord<Integer, String>,Void>{
+
+        @ProcessElement
+        public void processElement(ProcessContext  c) {
+
+            KafkaRecord<Integer, String> record = c.element();
+            KV<Integer,String> record2 =  record.getKV();
+
+            Long processed_tmp  = record.getTimestamp();
+            String topic        = record.getTopic();
+            Integer key         = record2.getKey();
+            String value        = record2.getValue();
+
+            System.out.println("Topic = " + topic + " Fed to beam at unix epoch = " + processed_tmp + " message key " + key + " message content " + value );
+        }
+    }
+}
