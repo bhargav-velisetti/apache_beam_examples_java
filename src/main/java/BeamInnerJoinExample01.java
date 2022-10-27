3386e84e63488a865715d45e6111d73bf69f2720
Bhargav Velisetti <bhargava.velisetti@gmail.com>
2022-10-27 11:26:01 -0700

    flattend innerjoin pcol

--- src/main/java/BeamInnerJoinExample01.java
+++ src/main/java/BeamInnerJoinExample01.java
@@ -6,9 +6,13 @@ import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.schemas.transforms.Join;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.Row;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BeamInnerJoinExample01 {
 
@@ -26,17 +30,56 @@ public class BeamInnerJoinExample01 {
         PCollection<Row>    pc_left  = pc2.apply(SqlTransform.query("select Ship_name, Age from PCOLLECTION"));
         PCollection<Row>    pc_right = pc2.apply(SqlTransform.query("select Ship_name, Tonnage, cabins, crew from PCOLLECTION"));
 
-        System.out.println(pc_right.getSchema().toString());
-        System.out.println(pc_left.getSchema().toString());
+        List<Schema.Field> schema_left = pc_left.getSchema().getFields();
+        List<Schema.Field> schema_right = pc_right.getSchema().getFields();
 
+
+        List<Schema.Field> schema_joined_list = new ArrayList<>();
+        for(int i=0;i<schema_left.size();i++){
+            schema_joined_list.add(schema_left.get(i));
+        }
+        for(int i=1;i<schema_right.size();i++){
+            schema_joined_list.add(schema_right.get(i));
+        }
+
+        Schema schemaJoined = Schema.builder().addFields(schema_joined_list).build();
+        System.out.println(schemaJoined.toString());
         PCollection<Row> joined = pc_left.apply(Join.<Row,Row>innerJoin(pc_right).using("Ship_name"));
 
-        joined.apply(ParDo.of(new ReadCsvExample01.RowPrinter()));
+        PCollection<Row> flatndJoin = joined.apply(ParDo.of(new FlattenJoinedPcol(schemaJoined)))
+                                            .setRowSchema(schemaJoined);
+
+        flatndJoin.apply(ParDo.of(new ReadCsvExample01.RowPrinter()));
+        System.out.println(flatndJoin.getSchema().toString());
 
 
         PipelineResult result = p.run();
         result.waitUntilFinish();
-
-
     }
+
+    public static class FlattenJoinedPcol extends DoFn<Row,Row> {
+
+        static Schema schema;
+        public FlattenJoinedPcol(Schema schema) {
+            this.schema = schema;
+        }
+
+        @ProcessElement
+        public void processElement(ProcessContext c) {
+
+            Row outer_row = c.element();
+            Row lhs_row = outer_row.getRow("lhs");
+            Row rhs_row = outer_row.getRow("rhs");
+
+            Row.Builder joinedRow = Row.withSchema(schema);
+            joinedRow.addValues(lhs_row.getValues());
+
+            for (int i=1;i<rhs_row.getFieldCount();i++) {
+                joinedRow.addValue(rhs_row.getValue(i));
+            }
+            c.output(joinedRow.build()); ;
+
+        }
+    }
+
 }
